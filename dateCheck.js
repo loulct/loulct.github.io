e2e4aee2d05169248efdff789ab92aece7f6d255
lou025 <wrallday@gmail.com>
2024-07-08 22:04:09 +0200

    add dateCheck.js

---
+++ dateCheck.js
@@ -0,0 +1,7 @@
+export const birthday = new Date(2000, 7, 18);
+
+export function calculateAge(birthday) {
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
